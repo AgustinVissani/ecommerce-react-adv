6ed9937a49f19e8792bfe6c4f4de3c27709b9f4d
AgustinVissani <vissani1997@gmail.com>
2023-12-01 14:50:09 +0100

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { Filters } from './Filters.jsx'
+import '../css/Header.css';
 
 export function Header () {
   return (
     <header>
-      <h1>React Shop 🛒</h1>
+      <h1 className='title' >React Shop 🛒</h1>
       <Filters />
     </header>
   )
