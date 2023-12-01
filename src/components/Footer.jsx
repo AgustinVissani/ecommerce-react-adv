3baa2c135e95423b9f6cd60a35dcc142f0b24cf7
AgustinVissani <vissani1997@gmail.com>
2023-12-01 12:32:40 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import '../css/Footer.css'
+
+export function Footer () {
+  return (
+    <footer className='footer'>
+      <h4>React ⚛️ － <span>@AgustinVissani</span></h4>
+      <h5>Shopping Cart</h5>
+    </footer>
+  )
+}
